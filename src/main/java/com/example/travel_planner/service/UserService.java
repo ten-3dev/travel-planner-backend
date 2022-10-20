b2aec4c46c71c3155e4500be34bcdd5782785b24
mouse <gkdlfn032@gmail.com>
2022-10-20 15:25:56 +0900

    비번찾기 수정중

--- src/main/java/com/example/travel_planner/service/UserService.java
+++ src/main/java/com/example/travel_planner/service/UserService.java
@@ -221,7 +221,7 @@ public class UserService {
     public ResponseEntity passwordChange(Map<String, String> email) {
         Optional<Users> resultEmail = userRepository.findById(email.get("email"));
         if (resultEmail.isPresent()) {
-            if(email.get("pw").equals(email.get("pwRe"))){
+//            if(email.get("pw").equals(email.get("pwRe"))){
                 Users users = Users.builder()
                         .email(resultEmail.get().getEmail())
                         .name(resultEmail.get().getName())
@@ -232,9 +232,9 @@ public class UserService {
                         .build();
                 userRepository.save(users);
                 return new StatusCode(HttpStatus.OK, "비밀번호 변경").sendResponse();
-            }else{
-                return new StatusCode(HttpStatus.BAD_REQUEST, "비밀번호가 다릅니다.").sendResponse();
-            }
+//            }else{
+//                return new StatusCode(HttpStatus.BAD_REQUEST, "비밀번호가 다릅니다.").sendResponse();
+//            }
 
         } else {
             return new StatusCode(HttpStatus.BAD_REQUEST, "없는 이메일 입니다").sendResponse();
