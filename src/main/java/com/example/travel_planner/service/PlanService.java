575beef68764d30d49311790cc67cad2b3b3050f
jjijju0918 <jisu7032@naver.com>
2022-10-30 12:15:35 +0900

    하

--- src/main/java/com/example/travel_planner/service/PlanService.java
+++ src/main/java/com/example/travel_planner/service/PlanService.java
@@ -124,8 +124,8 @@ public class PlanService {
         }
     }
 
-    public ResponseEntity getPlan(String id){
-    List<Plans> plans =  planRepository.getPlansById(id);
+    public ResponseEntity getPlan(String email){
+    List<Plans> plans =  planRepository.getPlansById(email);
         return new StatusCode(HttpStatus.OK, plans, "공유된플랜보기 조회성공").sendResponse();
     }
 }
